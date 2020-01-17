63a1c5439000e785db1869e47eb0b79857ae5042
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-17 22:08:49 +0530

    added new location button and its router

--- controller/controller_location.js
+++ controller/controller_location.js
@@ -10,3 +10,15 @@ exports.city_list = function (req, res) {
     res.locals = {  title: 'City' };
     res.render('Location/city');
 };
+exports.add_country = function (req, res) {
+    res.locals = {  title: 'Add Country' };
+    res.render('Location/add_country');
+};
+exports.add_state = function (req, res) {
+    res.locals = {  title: 'Add State' };
+    res.render('Location/add_state');
+};
+exports.add_city = function (req, res) {
+    res.locals = {  title: 'Add City' };
+    res.render('Location/add_city');
+};
--- routes/location.js
+++ routes/location.js
@@ -5,10 +5,11 @@ const controller_location = require('../controller/controller_location');
 
 //user-roles
 router.get('/country', controller_location.country_list);
-
 router.get('/state', controller_location.state_list);
-
 router.get('/city', controller_location.city_list);
+router.get('/add-country', controller_location.add_country);
+router.get('/add-state', controller_location.add_state);
+router.get('/add-city', controller_location.add_city);
 
 
 module.exports = router;
