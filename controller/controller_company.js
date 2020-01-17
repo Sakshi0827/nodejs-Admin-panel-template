4ad72aeac13462d2146a7669808af28a19cf9f25
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-17 22:42:31 +0530

    added new-company button and its router

--- controller/controller_company.js
+++ controller/controller_company.js
@@ -2,3 +2,7 @@ exports.company_list = function (req, res) {
     res.locals = {  title: 'Company' };
     res.render('Company/company');
 };
+exports.add_company = function (req, res) {
+    res.locals = {  title: 'Add Company' };
+    res.render('Company/add_company');
+};
--- routes/company.js
+++ routes/company.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller_company = require('../controller/controller_company');
 
 router.get('/company', controller_company.company_list);
+router.get('/add-company', controller_company.add_company);
+
 
 module.exports = router;
