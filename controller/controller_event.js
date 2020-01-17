163aa4daecc85838e741226794dbe50f870f3bb2
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-17 22:27:09 +0530

    added new-event and new-event-category button and its router

--- controller/controller_event.js
+++ controller/controller_event.js
@@ -3,6 +3,14 @@ exports.event_list = function (req, res) {
     res.render('Event/event_list');
 };
 exports.event_category =  function (req, res) {
-    res.locals = {  title: 'Event List' };
+    res.locals = {  title: 'Event Category List' };
     res.render('Event/event_category');
+};
+exports.add_event =  function (req, res) {
+    res.locals = {  title: 'Add Event' };
+    res.render('Event/add_event.ejs');
+};
+exports.add_event_category =  function (req, res) {
+    res.locals = {  title: 'Add Event Category' };
+    res.render('Event/add_event_category.ejs');
 };
--- routes/events.js
+++ routes/events.js
@@ -5,5 +5,8 @@ const controller_event = require('../controller/controller_event');
 
 router.get('/event-list', controller_event.event_list);
 router.get('/event-category', controller_event.event_category);
+router.get('/add-event', controller_event.add_event);
+router.get('/add-event-category', controller_event.add_event_category);
+
 
 module.exports = router;
