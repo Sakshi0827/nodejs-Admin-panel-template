9001ed9b7993f483ec680389a47bbf2cf227ab5e
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-18 23:03:52 +0530

    added new-company and new-challenge form

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const controller_events = require('../controller/controller_blogs');
+const controller_blogs = require('../controller/controller_blogs');
 
-router.get('/blogs-list', controller_events.blogs_list);
-router.get('/blogs-category', controller_events.blogs_category);
-router.get('/add-blogs', controller_events.add_blogs);
-router.get('/add-blogs-category', controller_events.add_blogs_category);
+router.get('/blogs-list', controller_blogs.blogs_list);
+router.get('/blogs-category', controller_blogs.blogs_category);
+router.get('/add-blogs', controller_blogs.add_blogs);
+router.get('/add-blogs-category', controller_blogs.add_blogs_category);
 
 
 module.exports = router;
