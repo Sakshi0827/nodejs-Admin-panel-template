a5a86dc9919b6be07164e22fc337e40d163c4e89
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-19 23:42:40 +0530

    added post() for add new fitness group

--- controller/controller_fitness_goup.js
+++ controller/controller_fitness_goup.js
@@ -6,3 +6,9 @@ exports.add_fitnessGroup = function (req, res) {
     res.locals = {  title: 'Add Fitness Group' };
     res.render('Fitness-group/add_fitness_group');
 };
+exports.add_fitnessGroup_post = function (req, res) {
+    const fitnessGroupName = req.body.fitnessGroupName;
+    console.log(req.body);
+    //DB
+    res.redirect('/fitness-group');
+};
--- routes/fitness_group.js
+++ routes/fitness_group.js
@@ -5,6 +5,7 @@ const controller_fitnessGroup = require('../controller/controller_fitness_goup')
 
 router.get('/fitness-group', controller_fitnessGroup.fitnessGroup_list);
 router.get('/add-fitness-group', controller_fitnessGroup.add_fitnessGroup);
+router.post('/add-fitness-group', controller_fitnessGroup.add_fitnessGroup_post);
 
 
 module.exports = router;
