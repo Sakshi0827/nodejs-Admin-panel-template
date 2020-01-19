bc04481d1c7c02ff5c091b98ce81cc726436efac
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-19 23:37:35 +0530

    added post() for add new challenges

--- controller/controller_challenges.js
+++ controller/controller_challenges.js
@@ -6,3 +6,11 @@ exports.add_challenges = function (req, res) {
     res.locals = {  title: 'Add Challenges' };
     res.render('Challenges/add_challenges');
 };
+exports.add_challenges_post = function (req, res) {
+    const challengeTilte = req.body.challengeTilte;
+    const challengePrice = req.body.challengePrice;
+    const challengeDescription = req.body.challengeDescription;
+    const challengeNote = req.body.challengeNote;
+    console.log(req.body);
+    res.redirect('/challenges');
+};
