2c59b331dd9b629a5bb17b45458e67a820de240f
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-20 11:37:00 +0530

    added post() for add-event-category

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const controller_event = require('../controller/controller_event');
 
+
 router.get('/event-list', controller_event.event_list);
 router.get('/event-category', controller_event.event_category);
 router.get('/add-event', controller_event.add_event);
