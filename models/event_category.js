c61211e150d7a2c6a150ed9e142f38f00468229a
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-21 14:43:04 +0530

    added event_category.js models

---
+++ models/event_category.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+const connection = require('../config/configdb');
+const sequelize = connection.connection;
+
+// console.log("Sequelize object", sequelize);
+
+const Model = Sequelize.Model;
+
+
+class Event_category extends Model {};
+Event_category.init({
+        // attributes
+        fitness_group_id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        fitness_group_name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        created_date: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        updated_date: {
+            type: Sequelize.DATE,
+            allowNull: false
+        }},
+    {
+        sequelize,
+        modelName: 'event_category_master'
+        // options
+    });
+
+module.exports = { Event_category };
