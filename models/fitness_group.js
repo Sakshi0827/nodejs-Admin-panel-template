87aff78f4d8406ed95229d1a2dc495e090f4b57a
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-21 14:38:17 +0530

    added fitness_group.js models

---
+++ models/fitness_group.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+const connection = require('../config/configdb');
+const sequelize = connection.connection;
+
+// console.log("Sequelize object", sequelize);
+
+const Model = Sequelize.Model;
+
+
+class Fitness_group extends Model {};
+Fitness_group.init({
+        // attributes
+        fitness_group_id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        fitness_group_name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        created_date: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        updated_date: {
+            type: Sequelize.DATE,
+            allowNull: false
+        }},
+    {
+        sequelize,
+        modelName: 'fitness_group_master'
+        // options
+    });
+
+module.exports = { Fitness_group };
