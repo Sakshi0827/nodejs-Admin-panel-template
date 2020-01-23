f6594f32abcc57675600dfe23af105168aac339a
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-01-23 14:31:36 +0530

    _______

--- controller/controller_blogs.js
+++ controller/controller_blogs.js
@@ -7,7 +7,44 @@ exports.blogs_list = function (req, res) {
 
 exports.blogs_category =  function (req, res) {
     res.locals = {  title: 'Blog Category' };
-    res.render('Blogs/blogs_category');
+
+    try{
+
+            Blogs_category.sync({ force: false }).then((result) => {
+            console.log("Result of sync", result);
+            Blogs_category.findAll({ }).then(blogs_category => {
+                console.log("All blogs_category:", JSON.stringify(blogs_category, null, 4));
+                
+                if(!blogs_category.length){
+                    return res.json({
+                        status: 404,                        
+                        message: "blogs_category not found."
+                    })    
+                }
+                return res.render('Blogs/blogs_category', {
+                    status: 200,
+                    data: blogs_category,
+                    message: "blogs_category fetched successfully."
+                })
+            })
+
+        })
+            .catch(err => {
+                console.error('Unable to connect to the database:', err);
+                return res.json({
+                    status: 500,
+                    data: err,
+                    message: "company fetching failed."
+                })
+            });
+        } catch (exception){
+            console.log("An exception occured, please contact the administrator.", exception);
+        }
+
+
+
+
+
 };
 
 exports.add_blogs =  function (req, res) {
@@ -30,11 +67,14 @@ exports.add_blogs_category_post =  function (req, res) {
             req.body
         ).then(blogs_category_name => {
             console.log("New Blog Category's auto-generated ID:", blogs_category_name.blogs_category_id);
-            return res.json({
-                status: 200,
-                data: blogs_category_name,
-                message: "New Blogs Category created successfully."
-            })
+            // return res.json({
+            //     status: 200,
+            //     data: blogs_category_name,
+            //     message: "New Blogs Category created successfully."
+            // })
+
+            return res.redirect('/blogs-category');
+
         }).catch(err => {
             console.error('Unable to connect to the database:', err);
             return res.json({
