34dca4a4730d90af9ae47fc5c8328460a47d4fee
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-01-23 13:41:54 +0530

    updated fitness-group(controller, ejs) for fetch operation

--- controller/controller_fitness_goup.js
+++ controller/controller_fitness_goup.js
@@ -2,12 +2,41 @@ const Fitness_group = require('../models/fitness_group');
 
 exports.fitnessGroup_list = function (req, res) {
     res.locals = {  title: 'Fitness Group' };
-    res.render('Fitness-group/fitness_group');
+    try{
+        Fitness_group.findAll({ }).then(fitness_group => {
+            console.log("All Fitness Group:", JSON.stringify(fitness_group, null, 4));
+
+            if(!fitness_group.length){
+                return res.json({
+                    status: 404,
+                    message: "Fitness Group not found."
+                })
+            }
+            return res.render('Fitness-group/fitness_group', {
+                status: 200,
+                data: fitness_group,
+                message: "Fitness Group fetched successfully."
+            })
+        }).catch(err => {
+            console.error('Unable to connect to the database:', err);
+            return res.json({
+                status: 500,
+                data: err,
+                message: "Fitness Group fetching failed."
+            })
+        });
+    } catch (exception){
+        console.log("An exception occured, please contact the administrator.", exception);
+    }
 };
+
+
 exports.add_fitnessGroup = function (req, res) {
     res.locals = {  title: 'Add Fitness Group' };
     res.render('Fitness-group/add_fitness_group');
 };
+
+
 exports.add_fitnessGroup_post = function (req, res) {
     const fitness_group_name = req.body.fitness_group_name;
     console.log(req.body);
@@ -18,11 +47,12 @@ exports.add_fitnessGroup_post = function (req, res) {
             req.body
         ).then(fitness_group_name => {
             console.log("New Fitness Group's auto-generated ID:", fitness_group_name.fitness_group_id);
-            return res.json({
-                status: 200,
-                data: fitness_group_name,
-                message: "New Fitness Group created successfully."
-            })
+            // return res.json({
+            //     status: 200,
+            //     data: fitness_group_name,
+            //     message: "New Fitness Group created successfully."
+            // })
+            res.redirect('/fitness-group')
         }).catch(err => {
             console.error('Unable to connect to the database:', err);
             return res.json({
