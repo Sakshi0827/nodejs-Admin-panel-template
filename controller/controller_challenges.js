402d2cab44349dca058f5749c6593a98ac5a73ae
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-01-23 12:25:55 +0530

    changes in controller_challenges' post method

--- controller/controller_challenges.js
+++ controller/controller_challenges.js
@@ -1,3 +1,10 @@
+const connection = require('../config/configdb');
+const sequelize = connection.connection;
+
+const Challenges  = require('../models/challenges');
+
+
+
 exports.challenges_list = function (req, res) {
     res.locals = {  title: 'Challenges' };
     res.render('Challenges/challenges');
@@ -7,10 +14,41 @@ exports.add_challenges = function (req, res) {
     res.render('Challenges/add_challenges');
 };
 exports.add_challenges_post = function (req, res) {
-    const challengeTilte = req.body.challengeTilte;
-    const challengePrice = req.body.challengePrice;
-    const challengeDescription = req.body.challengeDescription;
-    const challengeNote = req.body.challengeNote;
+    const challenge_tilte = req.body.challenge_tilte;
+    const challenge_price = req.body.challenge_price;
+    const challenge_description = req.body.challenge_description;
+    const challenge_note = req.body.challenge_note;
     console.log(req.body);
-    res.redirect('/challenges');
+
+
+
+
+//database code to insert dataconsole.log("Attempting to add company.");
+    Challenges.sync({ force: false }).then((result) => {
+        console.log("Result of sync", result);
+        Challenges.create(
+            req.body
+        ).then(challenge_title => {
+            console.log("New challenge's auto-generated ID:", challenge_title.challenge_id);
+            return res.json({
+                status: 200,
+                data: challenge_title,challenge_price,
+                message: "challenge created successfully."
+            })
+        }).catch(err => {
+            console.error('Unable to connect to the database:', err);
+            return res.json({
+                status: 500,
+                data: err,
+                message: "Challenges creation failed."
+            })
+        });
+    }).catch((error) => {
+        console.log("An error was encountered during the synchronization", error);
+    })
+
+
+
+
+    // res.redirect('/challenges');
 };
