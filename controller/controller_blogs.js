82ad25fe157d3f885cf736e8e96a48d4ef153625
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-01-24 13:50:07 +0530

    changes in blogs model, index.js, controller blogs

--- controller/controller_blogs.js
+++ controller/controller_blogs.js
@@ -1,50 +1,45 @@
+const Blogs = require('../models/blogs');
 const Blogs_category = require('../models/blogs_category');
+
+const upload = require('../index');
 
 exports.blogs_list = function (req, res) {
     res.locals = {  title: 'Blog List' };
     res.render('Blogs/blogs_list');
+    console.log(upload);
 };
 
 exports.blogs_category =  function (req, res) {
     res.locals = {  title: 'Blog Category' };
-
     try{
-
-            Blogs_category.sync({ force: false }).then((result) => {
-            console.log("Result of sync", result);
-            Blogs_category.findAll({ }).then(blogs_category => {
-                console.log("All blogs_category:", JSON.stringify(blogs_category, null, 4));
-                
-                if(!blogs_category.length){
-                    return res.json({
-                        status: 404,                        
-                        message: "blogs_category not found."
-                    })    
-                }
-                return res.render('Blogs/blogs_category', {
-                    status: 200,
-                    data: blogs_category,
-                    message: "blogs_category fetched successfully."
-                })
-            })
-
-        })
-            .catch(err => {
-                console.error('Unable to connect to the database:', err);
-                return res.json({
-                    status: 500,
-                    data: err,
-                    message: "company fetching failed."
-                })
-            });
-        } catch (exception){
-            console.log("An exception occured, please contact the administrator.", exception);
+        Blogs_category.sync({ force: false }).then((result) => {
+        console.log("Result of sync", result);
+        Blogs_category.findAll({ }).then(blogs_category => {
+        console.log("All blogs_category:", JSON.stringify(blogs_category, null, 4));
+        if(!blogs_category.length){
+            return res.json({
+                status: 404,                        
+                message: "blogs_category not found."
+            })    
         }
-
-
-
-
-
+        return res.render('Blogs/blogs_category', {
+            status: 200,
+            data: blogs_category,
+            message: "blogs_category fetched successfully."
+        })
+    })
+    })
+    .catch(err => {
+    console.error('Unable to connect to the database:', err);
+    return res.json({
+        status: 500,
+        data: err,
+        message: "company fetching failed."
+    })
+    });
+} catch (exception){
+            console.log("An exception occured, please contact the administrator.", exception);
+    }
 };
 
 exports.add_blogs =  function (req, res) {
@@ -57,6 +52,31 @@ exports.add_blogs_category =  function (req, res) {
     res.render('Blogs/add_blogs_category');
 };
 
+//POST 
+
+exports.add_blogs_post =  function (req, res) {
+    res.locals = {  title: 'Add Blogs' };
+    Blogs.sync({ force: false }).then((result) => {
+        console.log("Result of sync", result);
+        Blogs.create(
+            req.body
+        ).then(blogs => {
+            console.log("New Blog's auto-generated ID:", blogs_name.blogs_id);
+            return res.redirect('/blogs-list');
+        }).catch(err => {
+            console.error('Unable to connect to the database:', err);
+            return res.json({
+                status: 500,
+                data: err,
+                message: "New Blogs creation failed."
+            })
+        });
+    }).catch((exception) => {
+        console.log("An exception was encountered during the synchronization", exception);
+    })
+};
+
+
 exports.add_blogs_category_post =  function (req, res) {
     // const blogs_category_name = req.body.blogs_category_name;
     // console.log(req.body);
@@ -67,14 +87,7 @@ exports.add_blogs_category_post =  function (req, res) {
             req.body
         ).then(blogs_category_name => {
             console.log("New Blog Category's auto-generated ID:", blogs_category_name.blogs_category_id);
-            // return res.json({
-            //     status: 200,
-            //     data: blogs_category_name,
-            //     message: "New Blogs Category created successfully."
-            // })
-
             return res.redirect('/blogs-category');
-
         }).catch(err => {
             console.error('Unable to connect to the database:', err);
             return res.json({
@@ -83,7 +96,7 @@ exports.add_blogs_category_post =  function (req, res) {
                 message: "New Blogs Category creation failed."
             })
         });
-    }).catch((error) => {
-        console.log("An error was encountered during the synchronization", error);
+    }).catch((exception) => {
+        console.log("An exception was encountered during the synchronization", exception);
     })
 };
---
+++ models/blogs.js
@@ -0,0 +1,63 @@
+const Sequelize = require('sequelize');
+const Blogs_category = require('./blogs_category');
+// const User = require('./user');
+
+const connection = require('../config/configdb');
+const sequelize = connection.connection;
+
+const Model = Sequelize.Model;
+
+
+class Blogs extends Model {};
+
+Blogs.init({
+        // attributes
+        blogs_id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        user_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        blogs_title: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        blogs_description: {
+            type: Sequelize.TEXT,
+            allowNull: false
+        },
+        blogs_post_date: {
+            type: Sequelize.DATEONLY,
+            allowNull: false
+        },
+        blogs_category_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        blogs_image: {
+            type: Sequelize.STRING
+        },
+        created_date: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: new Date()
+        },
+        updated_date: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: new Date()
+        }},
+    {
+        sequelize,
+        modelName: 'blogs_category_master'
+        // options
+    });
+
+Blogs_category.hasMany(Blogs, {foreignKey: 'blogs_category_id'});
+// User.hasMany(Blogs, {foreignKey: 'user_id'});
+
+module.exports = Blogs_category ;
