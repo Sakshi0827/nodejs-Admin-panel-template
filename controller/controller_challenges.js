d25986f56f10ec0d4648577a2044e1ac2e79beeb
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-01-30 13:10:28 +0530

    opened controllers

--- controller/controller_challenges.js
+++ controller/controller_challenges.js
@@ -1,65 +1,65 @@
-// const connection = require('../config/configdb');
-// const sequelize = connection.connection;
+const connection = require('../config/configdb');
+const sequelize = connection.connection;
 
-// const Challenges  = require('../models/challenges');
+const Challenges  = require('../models/challenges');
 
 
 
-// exports.challenges_list = function (req, res) {
-//     res.locals = {  title: 'Challenges' };
-//     try{
-//         Challenges.sync({ force: false }).then((result) => {
-//         console.log("Result of sync", result);
-//             Challenges.findAll({ }).then(challenges => {
-//                 console.log("All Challenges:", JSON.stringify(challenges, null, 4));
-//                 return res.render('Challenges/challenges', {
-//                     status: 200,
-//                     data: challenges,
-//                     message: "Challenges fetched successfully."
-//                 })
-//             })
-//         }).catch(err => {
-//                 console.error('Unable to connect to the database:', err);
-//                 return res.json({
-//                     status: 500,
-//                     data: err,
-//                     message: "Challenges fetching failed."
-//                 })
-//             });
-//         } catch (exception){
-//             console.log("An exception occured, please contact the administrator.", exception);
-//     }
-// };
+exports.challenges_list = function (req, res) {
+    res.locals = {  title: 'Challenges' };
+    try{
+        Challenges.sync({ force: false }).then((result) => {
+        console.log("Result of sync", result);
+            Challenges.findAll({ }).then(challenges => {
+                console.log("All Challenges:", JSON.stringify(challenges, null, 4));
+                return res.render('Challenges/challenges', {
+                    status: 200,
+                    data: challenges,
+                    message: "Challenges fetched successfully."
+                })
+            })
+        }).catch(err => {
+                console.error('Unable to connect to the database:', err);
+                return res.json({
+                    status: 500,
+                    data: err,
+                    message: "Challenges fetching failed."
+                })
+            });
+        } catch (exception){
+            console.log("An exception occured, please contact the administrator.", exception);
+    }
+};
 
-// exports.add_challenges = function (req, res) {
-//     res.locals = {  title: 'Add Challenges' };
-//     res.render('Challenges/add_challenges');
-// };
+exports.add_challenges = function (req, res) {
+    res.locals = {  title: 'Add Challenges' };
+    res.render('Challenges/add_challenges');
+};
 
-// exports.add_challenges_post = function (req, res) {
-//     const challenge_tilte = req.body.challenge_tilte;
-//     const challenge_price = req.body.challenge_price;
-//     const challenge_description = req.body.challenge_description;
-//     const challenge_note = req.body.challenge_note;
-//     console.log(req.body);
-//     //database code to insert dataconsole.log("Attempting to add company.");
-//     Challenges.sync({ force: false }).then((result) => {
-//         console.log("Result of sync", result);
-//         Challenges.create(
-//             req.body
+exports.add_challenges_post = function (req, res) {
+    const challenge_tilte = req.body.challenge_tilte;
+    const challenge_price = req.body.challenge_price;
+    const challenge_description = req.body.challenge_description;
+    const challenge_note = req.body.challenge_note;
+    console.log(req.body);
+    //database code to insert dataconsole.log("Attempting to add company.");
+    Challenges.sync({ force: false }).then((result) => {
+        console.log("Result of sync", result);
+        Challenges.create(
+            req.body
             
-//         ).then(challenge_title => {
-//             console.log("New challenge's auto-generated ID:", challenge_title.challenge_id);
-//             res.redirect('/challenges');
-//         }).catch(err => {
-//             console.error('Unable to connect to the database:', err);
-//             return res.json({
-//                 status: 500,
-//                 data: err,
-//                 message: "Challenges creation failed."
-//             })
-//         });
-//     }).catch((error) => {
-//         console.log("An error was encountered during the synchronization", error);
-//     })
-// };
+        ).then(challenge_title => {
+            console.log("New challenge's auto-generated ID:", challenge_title.challenge_id);
+            res.redirect('/challenges');
+        }).catch(err => {
+            console.error('Unable to connect to the database:', err);
+            return res.json({
+                status: 500,
+                data: err,
+                message: "Challenges creation failed."
+            })
+        });
+    }).catch((error) => {
+        console.log("An error was encountered during the synchronization", error);
+    })
+};
--- controller/controller_payment.js
+++ controller/controller_payment.js
@@ -1,16 +1,16 @@
-// exports.payment_list = function (req, res) {
-//     res.locals = {  title: 'Payment' };
-//     res.render('payment/payment');
+exports.payment_list = function (req, res) {
+    res.locals = {  title: 'Payment' };
+    res.render('payment/payment');
+};
+exports.add_payment = function (req, res) {
+    res.locals = {  title: 'Add Payment' };
+    res.render('payment/add_payment');
+};
+// exports.add_challenges_post = function (req, res) {
+//     const challengeTilte = req.body.challengeTilte;
+//     const challengePrice = req.body.challengePrice;
+//     const challengeDescription = req.body.challengeDescription;
+//     const challengeNote = req.body.challengeNote;
+//     console.log(req.body);
+//     res.redirect('/challenges');
 // };
-// exports.add_payment = function (req, res) {
-//     res.locals = {  title: 'Add Payment' };
-//     res.render('payment/add_payment');
-// };
-// // exports.add_challenges_post = function (req, res) {
-// //     const challengeTilte = req.body.challengeTilte;
-// //     const challengePrice = req.body.challengePrice;
-// //     const challengeDescription = req.body.challengeDescription;
-// //     const challengeNote = req.body.challengeNote;
-// //     console.log(req.body);
-// //     res.redirect('/challenges');
-// // };
