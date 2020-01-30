3a344730f3a5fcd7d6c5ab87e2bd856b6b74fea3
Sakshi2708 <khandelwalsakshi455@gmail.com>
2020-01-30 14:50:39 +0530

    blogs  category post incomplete

--- controller/controller_blogs.js
+++ controller/controller_blogs.js
@@ -1,9 +1,7 @@
 const Blogs = require('../models/blogs');
 const Blogs_category = require('../models/blogs_category');
-// const multer = require('multer');
-// const Sequelize = require('sequelize');
 
-// blogs
+// blogs list
 exports.blogs_list = function (req, res) {
     res.locals = {  title: 'Blog List' };
     try{
@@ -30,31 +28,7 @@ exports.blogs_list = function (req, res) {
     }
 };
 
-// blogs category
-exports.blogs_category =  function (req, res) {
-    res.locals = {  title: 'Blog Category' };
-    try{
-        Blogs_category.findAll({ }).then(blogs_category => {
-        console.log("All blogs_category:", JSON.stringify(blogs_category, null, 4));
-        return res.render('Blogs/blogs_category', {
-            status: 200,
-            data: blogs_category,
-            message: "blogs_category fetched successfully."
-        })
-    }).catch(err => {
-    console.error('Unable to connect to the database:', err);
-        return res.json({
-            status: 500,
-            data: err,
-            message: "company fetching failed."
-        })
-    });
-    } catch (exception){
-            console.log("An exception occured, please contact the administrator.", exception);
-    }
-};
-
-// add BLOGS
+// add BLOGS get
 exports.add_blogs =  function (req, res) {
     res.locals = {  title: 'Add Blogs' };
     try{
@@ -78,20 +52,9 @@ exports.add_blogs =  function (req, res) {
     }
 };
 
-// add blogs category
-exports.add_blogs_category =  function (req, res) {
-    res.locals = {  title: 'Add Blogs Category' };
-    res.render('Blogs/add_blogs_category');
-};
-
-
-//POST 
-
-// add blogs post
+//add blogs post
 exports.add_blogs_post =  (req, res) =>{
     res.locals = {  title: 'Add Blogs' };
-    console.log("file ---------------", req.file);
-    console.log("BODY ---------------", req.body);
     Blogs.create(
         {
             user_id: 1,
@@ -103,7 +66,6 @@ exports.add_blogs_post =  (req, res) =>{
             blogs_image: req.file.filename
         }
         ).then(blogs => {
-            console.log("JSON-------------", blogs);
             console.log("New Blog's auto-generated ID:", blogs.blogs_id);
             return res.redirect('/blogs-list');
         }).catch(err => {
@@ -117,6 +79,45 @@ exports.add_blogs_post =  (req, res) =>{
             console.log("An exception was encountered during the synchronization", exception);
     })
 };
+
+
+
+// blogs category list
+exports.blogs_category =  function (req, res) {
+    res.locals = {  title: 'Blog Category' };
+    try{
+        Blogs_category.findAll({ }).then(blogs_category => {
+        console.log("All blogs_category:", JSON.stringify(blogs_category, null, 4));
+        return res.render('Blogs/blogs_category', {
+            status: 200,
+            data: blogs_category,
+            message: "blogs_category fetched successfully."
+        })
+    }).catch(err => {
+    console.error('Unable to connect to the database:', err);
+        return res.json({
+            status: 500,
+            data: err,
+            message: "company fetching failed."
+        })
+    });
+    } catch (exception){
+            console.log("An exception occured, please contact the administrator.", exception);
+    }
+};
+
+
+
+// add blogs category get
+exports.add_blogs_category =  function (req, res) {
+    res.locals = {  title: 'Add Blogs Category' };
+    res.render('Blogs/add_blogs_category');
+};
+
+
+
+
+//add blogs category POST 
 
 
 exports.add_blogs_category_post =  function (req, res) {
