ef704cba362036681315d9fe293829219845d7e9
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-02-05 13:57:53 +0530

    updated index.js

--- routes/router.js
+++ routes/router.js
@@ -15,10 +15,6 @@ router.get('/', function (req, res) {
     res.locals = {  title: 'Dashboard' };
     res.render('Dashboard/dashboard');
 });
-router.get('/form-editors', function (req, res) {
-    res.locals = {  title: 'Form Editor' };
-    res.render('Forms/form_editors');
-});
 // User
 router.use('/', userRouter);
 // // Location
