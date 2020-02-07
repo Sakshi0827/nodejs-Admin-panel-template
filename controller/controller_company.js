fbc9d42c3ccf4712b1a78e951d4f658a49faa452
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-02-07 13:38:54 +0530

    edit company completed

--- controller/controller_company.js
+++ controller/controller_company.js
@@ -53,40 +53,92 @@ exports.add_company_post = function (req, res) {
 
 //delete company
 exports.delete_company = function (req, res){
-        console.log(`Attempting to destroy a company with company_id ${req.params.company_id}`);
-        Company.destroy({
-            where: {
-                company_id: req.params.company_id
-            }
-        }).then((result) => {
-            if(result){
-                console.log("The Company was deleted.", result);
-                return res.json({
-                    status: 200,
-                    data: result,
-                    message: "Company delete successful."
-                })
-            } else {
-                console.log("Company delete failed.", result)
-                return res.json({
-                    status: 404,
-                    data: result,
-                    message: "Company delete failed, no record found to delete."
-                })
-            }
-        }).catch(err => {
-            console.error('Unable to connect to the database:', err);
+    console.log(`Attempting to destroy a company with company_id ${req.params.company_id}`);
+    Company.destroy({
+        where: {
+            company_id: req.params.company_id
+        }
+    }).then((result) => {
+        if(result){
+            console.log("The Company was deleted.", result);
             return res.json({
-                status: 500,
-                data: err,
-                message: "Company deletion failed."
+                status: 200,
+                data: result,
+                message: "Company delete successful."
             })
-        });
-    };
+        } else {
+            console.log("Company delete failed.", result)
+            return res.json({
+                status: 404,
+                data: result,
+                message: "Company delete failed, no record found to delete."
+            })
+        }
+    }).catch(err => {
+        console.error('Unable to connect to the database:', err);
+        return res.json({
+            status: 500,
+            data: err,
+            message: "Company deletion failed."
+        })
+    });
+};
 
 //edit company get
 exports.edit_company = function(req, res) {
     res.locals = {  title: 'Edit Company' };
     console.log(req.params);
-    res.render('Company/edit_company');   
-}
+    try{
+        Company.findAll({ where: {company_id: req.params.company_id } }).then(company => {
+            console.log("company with company id: ",req.params.company_id, " is", JSON.stringify(company, null, 4));
+            return res.render('Company/edit_company', {
+                status: 200,
+                data: company,
+                message: "company fetched successfully."
+            })
+        }).catch(err => {
+            console.error('Unable to connect to the database:', err);
+            return res.json({
+                status: 500,
+                data: err,
+                message: "company fetching failed."
+            })
+        });
+    } catch (exception){
+        console.log("An exception occured, please contact the administrator.", exception);
+    }
+};
+
+//edit company put
+exports.edit_company_put = function(req, res) {
+    res.locals = {title: 'Edit Company'};
+    console.log("------------",req.params, req.body);
+    Company.findOne({ where: { company_id: req.params.company_id }})
+        .then((result) => {
+            if(result){
+                result.update({
+                    company_name:req.body.company_name
+                });
+                console.log("The Company was edited.", result);
+                return res.json({
+                    status: 200,
+                    data: result,
+                    message: "Company edit successful."
+                })
+            } else {
+                console.log("Company edit failed.", result);
+                return res.json({
+                    status: 404,
+                    data: result,
+                    message: "Company edit failed, no record found to edit."
+                })
+            }
+        }).catch(err => {
+        console.error('Unable to connect to the database:', err);
+        return res.json({
+            status: 500,
+            data: err,
+            message: "Company edit failed."
+        })
+    });
+};
--- routes/company.js
+++ routes/company.js
@@ -8,5 +8,6 @@ router.get('/add-company', controller_company.add_company);
 router.post('/add-company', controller_company.add_company_post);
 router.delete('/delete-company:company_id', controller_company.delete_company);
 router.get('/edit-company:company_id', controller_company.edit_company);
+router.put('/edit-company:company_id', controller_company.edit_company_put);
 
 module.exports = router;
