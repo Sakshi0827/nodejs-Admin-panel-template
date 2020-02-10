12d2f9ad348b32b7da2e1e445697bdb9cbeb43c7
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-02-10 11:52:06 +0530

    created fitness group edit

--- controller/controller_fitness_group.js
+++ controller/controller_fitness_group.js
@@ -1,5 +1,6 @@
 const Fitness_group = require('../models/fitness_group');
 
+// fitness Group list
 exports.fitnessGroup_list = function (req, res) {
     res.locals = {  title: 'Fitness Group' };
     try{
@@ -90,6 +91,64 @@ exports.delete_fitnessGroup = function (req, res){
             status: 500,
             data: err,
             message: "fitness group deletion failed."
+        })
+    });
+};
+
+// edit Fitness group get
+exports.edit_fitnessGroup = function (req, res) {
+    res.locals = {  title: 'Edit Fitness Group' };
+    try{
+        Fitness_group.findAll({ where: {fitness_group_id: req.params.fitness_group_id }}).then(fitness_group => {
+            // console.log("All Fitness Group:", JSON.stringify(fitness_group, null, 4));
+            return res.render('Fitness-group/edit_fitness_group', {
+                status: 200,
+                data: fitness_group,
+                message: "Fitness Group fetched successfully."
+            })
+        }).catch(err => {
+            console.error('Unable to connect to the database:', err);
+            return res.json({
+                status: 500,
+                data: err,
+                message: "Fitness Group fetching failed."
+            })
+        })
+    }
+    catch (exception){
+        console.log("An exception occured, please contact the administrator.", exception);
+    }
+};
+
+// Edit fitness group put
+exports.edit_fitnessGroup_put =  function (req, res) {
+    res.locals = {title: 'Edit Fitness Group'};
+    Fitness_group.findOne({ where: { fitness_group_id: req.params.fitness_group_id  }})
+        .then((result) => {
+            if(result){
+                result.update({
+                    fitness_group_name:req.body.fitness_group_name,
+                });
+                console.log("The fitness_group_name was edited.", result);
+                return res.json({
+                    status: 200,
+                    data: result,
+                    message: "fitness_group_name edit successful."
+                })
+            } else {
+                console.log("fitness_group_name edit failed.", result);
+                return res.json({
+                    status: 404,
+                    data: result,
+                    message: "fitness_group_name edit failed, no record found to edit."
+                })
+            }
+        }).catch(err => {
+        console.error('Unable to connect to the database:', err);
+        return res.json({
+            status: 500,
+            data: err,
+            message: "fitness_group_name edit failed."
         })
     });
 };
--- routes/fitness_group.js
+++ routes/fitness_group.js
@@ -7,6 +7,8 @@ router.get('/fitness-group', controller_fitnessGroup.fitnessGroup_list);
 router.get('/add-fitness-group', controller_fitnessGroup.add_fitnessGroup);
 router.post('/add-fitness-group', controller_fitnessGroup.add_fitnessGroup_post);
 router.delete('/delete-fitness-group:fitness_group_id', controller_fitnessGroup.delete_fitnessGroup);
+router.get('/edit-fitness-group:fitness_group_id', controller_fitnessGroup.edit_fitnessGroup);
+router.put('/edit-fitness-group:fitness_group_id', controller_fitnessGroup.edit_fitnessGroup_put);
 
 
 module.exports = router;
