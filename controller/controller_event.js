2be12ee75ab352fec1b18c1c9c2e8f17aa79a815
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-02-14 23:54:43 +0530

    added event_date to event controller

--- controller/controller_event.js
+++ controller/controller_event.js
@@ -8,15 +8,15 @@ exports.event_list = function (req, res) {
     res.locals = {  title: 'Event List' };
     try{
         Event.findAll({  
-          include: [
-          {
-              model: City
-          },
-              {
+            include: [
+                {
+                  model: City
+                },
+                {
                   model:Event_category
-              }
-      ]
-  }).then(event => {
+                }
+            ]
+        }).then(event => {
             console.log("All event:", JSON.stringify(event, null, 4));
             return res.render('Event/event_list', {
                 status: 200,
@@ -33,6 +33,11 @@ exports.event_list = function (req, res) {
         });
     } catch (exception){
         console.log("An exception occured, please contact the administrator.", exception);
+        return res.json({
+            status: 500,
+            data: exception,
+            message: "Event fetching failed."
+        })
     }
 };
 
@@ -41,9 +46,9 @@ exports.add_event = function (req, res) {
     res.locals = {  title: 'Add Event' };
     try{
         City.findAll({ }).then(city => {
-            console.log("All city:", JSON.stringify(city, null, 4));
+            console.log("All cities:", JSON.stringify(city, null, 4));
             Event_category.findAll({ }).then(event_category => {
-                console.log("All event_category:", JSON.stringify(event_category, null, 4));
+                console.log("All event categories:", JSON.stringify(event_category, null, 4));
                 return res.render('Event/add_event', {
                     status: 200,
                     data: event_category,
@@ -56,29 +61,28 @@ exports.add_event = function (req, res) {
             return res.json({
                 status: 500,
                 data: err,
-                message: "event_category fetching failed."
+                message: "City or Event category fetching failed."
             })
         })
     }
     catch (exception){
         console.log("An exception occurred, please contact the administrator.", exception);
+        return res.json({
+            status: 500,
+            data: exception,
+            message: "City or Event category fetching failed."
+        })
     }
 };
 
 // Add-event post
 exports.add_event_post =  function (req, res) {
-    console.log("<------------>",  JSON.stringify(req.body.event_category_id));
     try{
         Event.create(
                 req.body
         ).then(event => {
             console.log("New event's auto-generated ID:", event.event_id);
-            // return res.json({
-            //     status: 200,
-            //     data: event_category_name,
-            //     message: "event_category created successfully."
-            // })
-            res.redirect('/event-list');
+            res.redirect(200, '/event-list');
         })
         .catch(err => {
             console.error('Unable to connect to the database:', err);
@@ -91,15 +95,18 @@ exports.add_event_post =  function (req, res) {
     }
     catch(exception) {
         console.log("An error was encountered during the synchronization", exception);
+        return res.json({
+            status: 500,
+            data: exception,
+            message: "Event creation failed."
+        })
     }
 };
 
 
 //event delete
-
-
 exports.delete_event = function (req, res){
-    console.log(`Attempting to destroy an event with event_id ${req.params.event_id}`);
+    console.log('Attempting to destroy an event with event_id:', req.params.event_id);
     Event.destroy({
         where: {
             event_id: req.params.event_id
@@ -113,7 +120,7 @@ exports.delete_event = function (req, res){
                 message: "Event delete successful."
             })
         } else {
-            console.log("Event delete failed.", result)
+            console.log("Event delete failed.", result);
             return res.json({
                 status: 404,
                 data: result,
@@ -134,26 +141,29 @@ exports.delete_event = function (req, res){
 exports.event_category =  function (req, res) {
     res.locals = {  title: 'Event Category List' };
     try{
-         Event_category.findAll({ }).then(event_category => {
-                console.log("All event_category:", JSON.stringify(event_category, null, 4));
-                return res.render('Event/event_category', {
-                    status: 200,
-                    data: event_category,
-                    message: "event_category fetched successfully."
-                })
+        Event_category.findAll({ }).then(event_category => {
+        console.log("All event_category:", JSON.stringify(event_category, null, 4));
+            return res.render('Event/event_category', {
+                status: 200,
+                data: event_category,
+                message: "event_category fetched successfully."
             })
-            .catch(err => {
-                console.error('Unable to connect to the database:', err);
-                return res.json({
-                    status: 500,
-                    data: err,
-                    message: "event_category fetching failed."
-                })
-            });
-        } catch (exception){
-            console.log("An exception occured, please contact the administrator.", exception);
-        }
-
+        }).catch(err => {
+            console.error('Unable to connect to the database:', err);
+            return res.json({
+                status: 500,
+                data: err,
+                message: "event category fetching failed."
+            })
+        })
+    } catch (exception){
+        console.log("An exception occured, please contact the administrator.", exception);
+        return res.json({
+            status: 500,
+            data: exception,
+            message: "event category fetching failed."
+        })
+    }
 };
 
 // event_category get
@@ -162,55 +172,53 @@ exports.add_event_category =  function (req, res) {
     res.render('Event/add_event_category.ejs');
 };
 
-
-
 // event_category post
 exports.add_event_category_post =  function (req, res) {
+    try {
         Event_category.create(
             req.body
         ).then(event_category_name => {
-            console.log("New event_category's auto-generated ID:", event_category_name.event_category_id);
-            // return res.json({
-            //     status: 200,
-            //     data: event_category_name,
-            //     message: "event_category created successfully."
-            // })
-            res.redirect('/event-category');
-        })
-        .catch(err => {
+            console.log("New event category's auto-generated ID:", event_category_name.event_category_id);
+            res.redirect(200, '/event-category');
+        }).catch(err => {
             console.error('Unable to connect to the database:', err);
             return res.json({
                 status: 500,
                 data: err,
-                message: "event_category creation failed."
+                message: "event category creation failed."
             })
         })
-    .catch((error) => {
-        console.log("An error was encountered during the synchronization", error);
-    })
+    } catch(exception) {
+        console.log("An error was encountered during the synchronization", exception);
+        return res.json({
+            status: 500,
+            data: exception,
+            message: "event category creation failed."
+        })
+    }
 };
 
 //delete event_category
 exports.delete_event_category = function (req, res){
-    console.log(`Attempting to destroy a event_category with event_category id: ${req.params.event_category_id}`);
+    console.log('Attempting to destroy a event category with event_category id: ', req.params.event_category_id);
     Event_category.destroy({
         where: {
             event_category_id: req.params.event_category_id
         }
     }).then((result) => {
         if(result){
-            console.log("The event_category was deleted.", result);
+            console.log("The event category was deleted.", result);
             return res.json({
                 status: 200,
                 data: result,
-                message: "event_category delete successful."
+                message: "event category delete successful."
             })
         } else {
-            console.log("event_category delete failed.", result);
+            console.log("event category delete failed.", result);
             return res.json({
                 status: 404,
                 data: result,
-                message: "event_category delete failed, no record found to delete."
+                message: "event category delete failed, no record found to delete."
             })
         }
     }).catch(err => {
@@ -218,7 +226,7 @@ exports.delete_event_category = function (req, res){
         return res.json({
             status: 500,
             data: err,
-            message: "event_category deletion failed."
+            message: "event category deletion failed."
         })
     });
 };
@@ -231,13 +239,13 @@ exports.edit_event = function(req, res) {
         Event.findAll({ where: {event_id: req.params.event_id } }).then(event => {
             console.log("event with event id: ",req.params.event_id, " is", JSON.stringify(event, null, 4));
             City.findAll({ }).then(city => {
-                console.log("All city:", JSON.stringify(city, null, 4));
+                console.log("All city: ", JSON.stringify(city, null, 4));
                 Event_category.findAll({ }).then(event_category => {
-                    console.log("All event_category:", JSON.stringify(event_category, null, 4));
+                    console.log("All event category: ", JSON.stringify(event_category, null, 4));
                     City.findAll({where:{city_id: event[0].city_id} }).then(city_result => {
-                        console.log("City-------", JSON.stringify(city_result));
+                        console.log("Event's City: ", JSON.stringify(city_result));
                         Event_category.findAll({where:{event_category_id: event[0].event_category_id} }).then(event_category_result => {
-                            console.log("City-------", JSON.stringify(event_category_result));
+                            console.log("Event's category: ", JSON.stringify(event_category_result));
                             return res.render('Event/edit_event', {
                                 status: 200,
                                 data: event,
@@ -261,6 +269,11 @@ exports.edit_event = function(req, res) {
         });
     } catch (exception){
         console.log("An exception occured, please contact the administrator.", exception);
+        return res.json({
+            status: 500,
+            data: err,
+            message: "event fetching failed."
+        })
     }
 };
 
@@ -268,7 +281,6 @@ exports.edit_event = function(req, res) {
 //edit event put
 exports.edit_event_put = function(req, res) {
     res.locals = {title: 'Edit Event'};
-    console.log("------------",req.params, req.body);
     Event.findOne({ where: { event_id: req.params.event_id }})
         .then((result) => {
             if(result){
@@ -302,8 +314,6 @@ exports.edit_event_put = function(req, res) {
     });
 };
 
-
-
 //edit event category get
 exports.edit_event_category = function(req, res) {
     res.locals = {  title: 'Edit Event Category' };
@@ -326,13 +336,17 @@ exports.edit_event_category = function(req, res) {
         });
     } catch (exception){
         console.log("An exception occured, please contact the administrator.", exception);
+        return res.json({
+            status: 500,
+            data: exception,
+            message: "event category fetching failed."
+        })
     }
 };
 
 //edit event category put
 exports.edit_event_category_put = function(req, res) {
     res.locals = {title: 'Edit Event Catgeory'};
-    console.log("------------",req.params, req.body);
     Event_category.findOne({ where: { event_category_id: req.params.event_category_id }})
         .then((result) => {
             if(result){
